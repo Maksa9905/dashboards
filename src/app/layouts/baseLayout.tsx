0add5b2abfd0ed7fc0cf2edb9183171569529494
Maksa9905 <Maksa9905@yandex.ru>
2025-04-08 02:20:05 +0300

    fix: added LIQN

--- src/app/layouts/baseLayout.tsx
+++ src/app/layouts/baseLayout.tsx
@@ -3,6 +3,7 @@ import { Outlet } from "react-router";
 export const BaseLayout = () => {
   return (
     <div className="p-6">
+      <span className="absolute bottom-2 left-2 text-neutral-10">LIQN</span>
       <Outlet />
     </div>
   );
