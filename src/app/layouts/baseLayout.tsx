7c711de29416243478019ffa66f010b28367f672
hakolr <m.gayvoronskiy@flang.io>
2025-04-08 16:29:30 +0300

    feat: fixed base layout position

--- src/app/layouts/baseLayout.tsx
+++ src/app/layouts/baseLayout.tsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router";
 export const BaseLayout = () => {
   return (
     <div className="p-6">
-      <span className="absolute bottom-2 left-2 text-neutral-10">LIQN</span>
+      <span className="fixed bottom-2 left-2 text-neutral-10 z-[1]">LIQN</span>
       <Outlet />
     </div>
   );
